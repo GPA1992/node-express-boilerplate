faab8732ea752cfa8b08b43f15de4eded48fa024
GPA1992 <gabrielproencaaraujo@gmail.com>
2023-10-15 18:08:33 -0300

    feat: compose e variaveis de ambiente para rodar localmente ajustados

--- build/db/config/config.js
+++ build/db/config/config.js
@@ -1,6 +1,5 @@
 "use strict";
 require("dotenv/config");
-console.log('user', process.env.DB_PORT);
 const config = {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
